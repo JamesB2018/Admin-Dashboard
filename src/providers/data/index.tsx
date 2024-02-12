c2de2425320b732029992288c67d5a83fd8eabc7
James Brake <brake.james@yahoo.com>
2024-02-12 06:28:24 -0600

    Added data and live Providers for webSocket

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,6 +1,13 @@
-import { GraphQLClient } from "@refinedev/nestjs-query"
+import graphqlDataProvider, {
+     GraphQLClient,
+     liveProvider as graphqlLiveProvider
+    } from "@refinedev/nestjs-query";
+import {createClient } from 'graphql-ws'
+import { fetchWrapper } from "./fetch-wrapper";
 
+export const API_BASE_URL = 'https://api.crm.refine.dev'
 export const API_URL = 'https://api.crm.refine.dev'
+export const WS_URL = 'wss://api.crm.refine.dev/graphql'
 
 export const client = new GraphQLClient(API_URL, {
     fetch: (url:string, options: RequestInit) => {
@@ -11,3 +18,26 @@ export const client = new GraphQLClient(API_URL, {
         }
     }
 })
+
+//Web Socket client. Built in provider Live Provider. lets app update in real time. 
+export const wsClient = typeof window !== "undefined"
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+            const accessToken = localStorage.getItem("access_token");
+
+            return {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                }
+            }
+        }
+
+    })
+    : undefined //In case we are not within the browser, we make it undefined
+
+//Creating a data provider variable for requests to the GraphQL API
+export const dataProvider = graphqlDataProvider(client) //Function that takes a graphql client and data provider for Refien to use
+
+//Creating a live provider for subscriptions
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined //if wsClient exists then call live Provider otherwise it's undefined
