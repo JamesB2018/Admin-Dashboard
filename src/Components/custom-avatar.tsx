5086923bda2ee82e97c5cbcddce8c56f57686f7f
James Brake <brake.james@yahoo.com>
2024-02-15 16:21:05 -0600

    Added CustomAvatar component

---
+++ src/Components/custom-avatar.tsx
@@ -0,0 +1,15 @@
+ import {Avatar as AntdAvatar} from 'antd'
+ 
+ const CustomAvatar = () => {
+  return (
+    <AntdAvatar
+    alt={'WHatever I want'}
+    size="small"
+    style={{ backgroundColor: '#87d068'}}
+    >
+        JB
+    </AntdAvatar>
+  )
+}
+
+export default CustomAvatar
