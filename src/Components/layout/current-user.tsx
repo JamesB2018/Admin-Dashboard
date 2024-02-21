4f44716ba598a7a1eae9c853bd8bd872ff5fb550
James Brake <brake.james@yahoo.com>
2024-02-20 20:30:02 -0600

    Updated current-user & custom-avatar

--- src/Components/layout/current-user.tsx
+++ src/Components/layout/current-user.tsx
@@ -1,6 +1,11 @@
 import { Popover, Button } from 'antd'
+import CustomAvatar from '../custom-avatar'
+import { useGetIdentity } from '@refinedev/core'
+
+import type { User } from '@/graphql/schema.types'
 
 const CurrentUser = () => {
+  const { data: user} = useGetIdentity<User>()
   return (
     <>
     <Popover 
@@ -10,6 +15,13 @@ const CurrentUser = () => {
         overlayStyle={{ zIndex: 999 }}
         
         >
+
+          <CustomAvatar
+            name={user?.name}
+            src={user?.avatarUrl}
+            size="default"
+            style={{ cursor: 'pointer'}}  
+          />
     </Popover>
     </>
 
